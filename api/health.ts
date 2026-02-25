916ea0b7cee5316bf407a36487aa6ea2257aa953
dayoemmanuelj <emmanueljimoh@gmail.com>
2026-02-25 22:18:22 +0100

    update all recent changes

--- api/health.ts
+++ api/health.ts
@@ -2,6 +2,9 @@ export default (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://ejtechnologies.com.ng');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
   if (req.method !== 'GET') {
     res.setHeader('Content-Type', 'text/plain');
     return res.status(405).end('Method not allowed');
--- api/subscribe.ts
+++ api/subscribe.ts
@@ -25,6 +25,9 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://ejtechnologies.com.ng');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
   await connectDB();
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
